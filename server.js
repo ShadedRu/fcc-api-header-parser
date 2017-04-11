535aed7e15b0a15e7ac0cae5b8a3bef4da24e9c4
Alexander <shadedvologda@gmail.com>
2017-04-11 16:30:46 +0000

    good ol commit

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+var express = require('express');
+//\([^)]+\)
+var app = express();
 
+app.get('/*', function(req, res) {
+    console.log(req.headers);
+    var client_ip = req.headers['x-forwarded-for'];
+    var client_language = req.headers['accept-language'].split(',')[0];
+    var client_software = req.headers['user-agent'].match(/\([^)]+\)/g)[0].replace(/[(|)]/g, '');
+    var response = {
+        ipaddress: client_ip,
+        language: client_language,
+        software: client_software
+    };
+    res.end(JSON.stringify(response));
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
